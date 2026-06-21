362ba5a1d00450c1da93bc3b0a6650d404587094
Tamoharini <tamoharini2005@gmail.com>
2026-06-21 19:27:12 +0530

    Your custom update message

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/IMS";
-
+const mongoURI = process.env.MONGO_URI;
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
